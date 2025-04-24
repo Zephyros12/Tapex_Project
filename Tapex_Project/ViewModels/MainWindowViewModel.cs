ad40b612b913eddbf95db4e8deb7d330b9728bf4
임규성 <rimrbtjd12@gmail.com>
2025-04-24 14:21:33 +0900

    파라미터 조절 수정

--- Tapex_Project/ViewModels/MainWindowViewModel.cs
+++ Tapex_Project/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Tapex_Project.Services;
+﻿using Tapex_Project.Models;
+using Tapex_Project.Services;
 
 namespace Tapex_Project.ViewModels;
 
@@ -7,10 +8,30 @@ public sealed class MainWindowViewModel : ViewModelBase
     public ImageViewModel ImageVM { get; }
     public ResultViewModel ResultVM { get; } = new();
     public ParameterViewModel ParameterVM { get; } = new();
+
+    public RelayCommand DetectCmd { get; }
+
     public string Title => "Tapex Project";
 
     public MainWindowViewModel(IFilePickerService picker)
     {
         ImageVM = new ImageViewModel(picker);
+
+        DetectCmd = new RelayCommand(_ => Detect(), _ => ImageVM.Current is not null);
+        ImageVM.PropertyChanged += (_, e) =>
+        {
+            if (e.PropertyName == nameof(ImageViewModel.Current))
+                DetectCmd.NotifyCanExecuteChanged();
+        };
+    }
+
+    private void Detect()
+    {
+        if (ImageVM.Current is not null) return;
+
+        var detector = new Detector();
+        var results = detector.Run(ImageVM.Current.FullBitmap, ParameterVM.Config);
+
+        ResultVM.Update(results);
     }
 }
