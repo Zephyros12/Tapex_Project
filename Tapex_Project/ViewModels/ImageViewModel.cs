207b47185c45cd423f80add59120ac655bc0ba68
임규성 <rimrbtjd12@gmail.com>
2025-05-02 09:35:46 +0900

    마스크 이미지 볼 수 있게 수정

--- Tapex_Project/ViewModels/ImageViewModel.cs
+++ Tapex_Project/ViewModels/ImageViewModel.cs
@@ -2,6 +2,12 @@
 using System.Windows.Input;
 using Tapex_Project.Models;
 using Tapex_Project.Services;
+using Tapex_Project.Views;
+using Av = Avalonia.Media.Imaging;
+using SD = System.Drawing;
+using System.IO;
+using Emgu.CV;
+using Emgu.CV.Util;
 
 namespace Tapex_Project.ViewModels
 {
@@ -11,20 +17,33 @@ namespace Tapex_Project.ViewModels
     public class ImageViewModel : ViewModelBase
     {
         private readonly IFilePickerService _picker;
-        private ImageModel? _current;
 
+        private ImageModel? _current;
         public ImageModel? Current
         {
             get => _current;
             private set => SetProperty(ref _current, value);
         }
 
+        private Av.Bitmap? _maskBitmap;
+        public Av.Bitmap? MaskBitmap
+        {
+            get => _maskBitmap;
+            private set
+            {
+                if (SetProperty(ref _maskBitmap, value))
+                    (ViewMaskCmd as RelayCommand)?.NotifyCanExecuteChanged();
+            }
+        }
+
         public ICommand LoadCmd { get; }
+        public ICommand ViewMaskCmd { get; }
 
         public ImageViewModel(IFilePickerService picker)
         {
             _picker = picker;
             LoadCmd = new RelayCommand(async _ => await LoadAsync());
+            ViewMaskCmd = new RelayCommand(_ => ShowMaskWindow(), _ => MaskBitmap != null);
         }
 
         private async Task LoadAsync()
@@ -34,6 +53,28 @@ namespace Tapex_Project.ViewModels
             {
                 Current = ImageModel.FromFile(files[0]);
             }
+        }
+
+        public void SetMask(Mat mat)
+        {
+            using var buf = new VectorOfByte();
+            CvInvoke.Imencode(".png", mat, buf);
+
+            using var ms = new MemoryStream(buf.ToArray());
+            MaskBitmap = new Av.Bitmap(ms);
+        }
+
+        public void ClearMask() => MaskBitmap = null;
+
+        private void ShowMaskWindow()
+        {
+            if (MaskBitmap == null) return;
+
+            var win = new MaskPreviewWindow
+            {
+                DataContext = this
+            };
+            win.Show();
         }
     }
 }
---
+++ Tapex_Project/Views/MaskPreviewWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Tapex_Project.Views;
+
+public partial class MaskPreviewWindow : Window
+{
+    public MaskPreviewWindow()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
